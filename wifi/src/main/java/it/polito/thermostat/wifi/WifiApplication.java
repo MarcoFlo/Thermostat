cc2474e2de70f15ca4def63f5cf59a1140b75f83
florianMarco <florian.marco@live.com>
2019-04-05 16:24:21 +0200

    esercitazione1-malnati-working

--- wifi/src/main/java/it/polito/thermostat/wifi/WifiApplication.java
+++ wifi/src/main/java/it/polito/thermostat/wifi/WifiApplication.java
@@ -1,10 +1,19 @@
 package it.polito.thermostat.wifi;
 
+import it.polito.thermostat.wifi.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 @SpringBootApplication
 public class WifiApplication {
+    @Bean
+    public ConcurrentHashMap<String, User> users() {
+        return new ConcurrentHashMap<>();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(WifiApplication.class, args);
