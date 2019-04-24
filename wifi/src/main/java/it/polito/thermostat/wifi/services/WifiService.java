b365924b7f9cc5720a00c504ecca9da6a0b321c6
florianMarco <florian.marco@live.com>
2019-04-24 12:38:45 +0200

    debug

--- wifi/src/main/java/it/polito/thermostat/wifi/services/WifiService.java
+++ wifi/src/main/java/it/polito/thermostat/wifi/services/WifiService.java
@@ -100,8 +100,10 @@ public class WifiService {
             Integer netNumber;
             result.append(execService.execute("wpa_cli -iwlan0 disconnect | wpa_cli -iwlan0 add_network"));
             netNumber = Integer.valueOf(result.toString());
-
-            execService.execute("wpa_cli -iwlan0 set_network " + netNumber + " auth_alg OPEN | wpa_cli -iwlan0 set_network " + netNumber + " key_mgmt WPA-PSK | wpa_cli -iwlan0 set_network " + netNumber + " psk '\"" + pw + "\"' | wpa_cli -iwlan0 set_network " + netNumber + " proto RSN | wpa_cli -iwlan0 set_network " + netNumber + " mode 0 | wpa_cli -iwlan0 set_network " + netNumber + " ssid '\"" + essid + "\"' | wpa_cli -iwlan0 select_network " + netNumber + " | wpa_cli -iwlan0 enable_network " + netNumber + " | wpa_cli -iwlan0 reassociate ");
+            logger.info(netNumber + " net number in connectNewNet");
+            result.setLength(0);
+            result.append(execService.execute("wpa_cli -iwlan0 set_network " + netNumber + " auth_alg OPEN | wpa_cli -iwlan0 set_network " + netNumber + " key_mgmt WPA-PSK | wpa_cli -iwlan0 set_network " + netNumber + " psk '\"" + pw + "\"' | wpa_cli -iwlan0 set_network " + netNumber + " proto RSN | wpa_cli -iwlan0 set_network " + netNumber + " mode 0 | wpa_cli -iwlan0 set_network " + netNumber + " ssid '\"" + essid + "\"' | wpa_cli -iwlan0 select_network " + netNumber + " | wpa_cli -iwlan0 enable_network " + netNumber + " | wpa_cli -iwlan0 reassociate ");
+            logger.info(result.toString() + "\n connectNewNet");
             return handleConnectResult(netNumber);
         }
         return false;
