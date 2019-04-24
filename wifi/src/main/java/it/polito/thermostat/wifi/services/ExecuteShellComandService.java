46d8ad0046da0183d3b59d444acfbdcbe86d8c96
florianMarco <florian.marco@live.com>
2019-04-24 12:44:50 +0200

    debug

--- wifi/src/main/java/it/polito/thermostat/wifi/services/ExecuteShellComandService.java
+++ wifi/src/main/java/it/polito/thermostat/wifi/services/ExecuteShellComandService.java
@@ -4,7 +4,6 @@ package it.polito.thermostat.wifi.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.*;
 
@@ -12,7 +11,6 @@ import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
 @Service
-@Transactional
 public class ExecuteShellComandService {
     boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
     private Logger logger = LoggerFactory.getLogger(this.getClass());
