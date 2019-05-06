8c1854f90471e078c792491c037ff5391541a407
florianMarco <florian.marco@live.com>
2019-05-06 10:49:24 +0200

    controllermd initial commit

---
+++ controllermd/src/main/java/it/polito/thermostat/controllermd/ControllermdApplication.java
@@ -0,0 +1,13 @@
+package it.polito.thermostat.controllermd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllermdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllermdApplication.class, args);
+    }
+
+}
