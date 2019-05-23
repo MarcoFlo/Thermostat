8c5f9aefe253c585b396d092579d46623ba7eb21
florianMarco <florian.marco@live.com>
2019-05-23 13:21:04 +0200

    - getAvailableNetIterator functioning

--- wifi/src/main/java/it/polito/thermostat/wifi/services/WifiService.java
+++ wifi/src/main/java/it/polito/thermostat/wifi/services/WifiService.java
@@ -62,7 +62,7 @@ public class WifiService {
                     result.append(execService.execute("iwlist wlan0 scan | grep ESSID"));
                     listNet = Arrays.asList(result.toString().split("\n"));
                     if (listNet.get(0).length() != 0) {
-                        mapAvailableNet.put(listNet.size(), listNet.stream().map(s -> s.substring(s.indexOf("\"") + 1, s.length() - 1)).filter(s -> s.length() < 50).collect(Collectors.toList()));
+                        mapAvailableNet.put(listNet.size(), listNet.stream().map(s -> s.substring(s.indexOf("\"") + 1, s.length() - 1)).filter(s -> s.length() < 50).distinct().collect(Collectors.toList()));
                     }
                     result.setLength(0);
                     count++;
