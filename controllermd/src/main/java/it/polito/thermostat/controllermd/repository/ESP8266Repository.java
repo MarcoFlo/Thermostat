50231da2acbeaac39ad4145b267b3c4d92e1b16b
florianMarco <florian.marco@live.com>
2019-05-30 20:20:41 +0200

    bug repository

--- controllermd/src/main/java/it/polito/thermostat/controllermd/repository/ESP8266Repository.java
+++ controllermd/src/main/java/it/polito/thermostat/controllermd/repository/ESP8266Repository.java
@@ -3,5 +3,5 @@ package it.polito.thermostat.controllermd.repository;
 import it.polito.thermostat.controllermd.entity.ESP8266;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ESP8266Repository extends CrudRepository<ESP8266, Integer> {
+public interface ESP8266Repository extends CrudRepository<ESP8266, String> {
 }
