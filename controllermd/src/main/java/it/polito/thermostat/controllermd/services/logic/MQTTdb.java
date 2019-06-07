d40b78057c380bce3ffb6bbdb4f8ddffdbc503b2
florianMarco <florian.marco@live.com>
2019-06-07 17:03:39 +0200

    - aws okay

--- controllermd/src/main/java/it/polito/thermostat/controllermd/services/logic/MQTTdb.java
+++ controllermd/src/main/java/it/polito/thermostat/controllermd/services/logic/MQTTdb.java
@@ -10,15 +10,8 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
-import java.nio.file.Files;
-
 import com.amazonaws.services.iot.client.sample.sampleUtil.SampleUtil.KeyStorePasswordPair;
-import org.springframework.util.ResourceUtils;
 
-
-/**
- * https://gist.github.com/jimrok/d25cb45b840f5a4ad700
- */
 
 @Service
 @PropertySource("classpath:aws-iot-sdk-samples.properties")
@@ -35,54 +28,34 @@ public class MQTTdb {
 
     @Value("${certificateFile}")
     String certificateFile;
-    //String certificateURL;
 
     @Value("${privateKeyFile}")
     String privateKeyFile;
-    //String privateKeyURL;
 
 
     private AWSIotMqttClient mqttClient;
 
     @PostConstruct
-    public void init() {
-
-//      logger.info(Objects.requireNonNull(ResourceUtils.getFile("./certificate//").list())[0]);
-
+    public void init() throws AWSIotException {
         KeyStorePasswordPair pair = SampleUtil.getKeyStorePasswordPair(certificateFile, privateKeyFile);
         mqttClient = new AWSIotMqttClient(clientEndpoint, clientId, pair.keyStore, pair.keyPassword);
-        mqttClient.setPort(8883);
-        mqttClient.setConnectionTimeout(100);
-        mqttClient.setKeepAliveInterval(100);
-        try {
-            logger.info("Trying to connect to aws");
+        mqttClient.connect();
+        mqttClient.subscribe(new NotificationTopic("pl19/notification", AWSIotQos.QOS1));
 
-            mqttClient.connect();
-            logger.info("done connect aws");
-        } catch (Exception e) {
-            logger.error("connect exception" + e.toString());
-        }
-
-        try {
-            mqttClient.subscribe(new MyTopic("pl19/notification", AWSIotQos.QOS1));
-        } catch (Exception e) {
-            logger.error("subscribe exception" + e.toString());
-        }
-        logger.info("mqttdb initialization done");
-
+        //mqttClient.publish("pl19/notification",AWSIotQos.QOS1, "prova");
     }
 
 
-    public static class MyTopic extends AWSIotTopic {
+    public static class NotificationTopic extends AWSIotTopic {
         private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-        public MyTopic(String topic, AWSIotQos qos) {
+        public NotificationTopic(String topic, AWSIotQos qos) {
             super(topic, qos);
         }
 
         @Override
         public void onMessage(AWSIotMessage message) {
-            logger.info(message.toString());
+            logger.info(message.getStringPayload());
 
         }
     }
