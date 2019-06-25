3813214530f12193b0ccf0c98177a0c96ff83d94
florianMarco <florian.marco@live.com>
2019-06-25 13:15:47 +0200

    - esp list

--- code/frontend/resources/static/js/phone.js
+++ code/frontend/resources/static/js/phone.js
@@ -1,9 +1,30 @@
 window.onload = function () {
+    var xhttp_esp = new XMLHttpRequest();
+    xhttp_esp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            espList = JSON.parse(xhttp_esp.responseText);
+
+            console.log(espList[0]);
+            console.log(espList.length);
+            var espcontainer = document.getElementById("esp-container");
+                for (i = 1; i < espList.length; i++) {
+                    var div = document.createElement("div");
+                    div.setAttribute("class","col-auto p-1");
+
+                    var button = document.createElement("button");
+                    button.setAttribute("type", "button");
+                    button.setAttribute("class", "btn btn-secondary");
+                    button.innerHTML = espList[i];
+                    button.setAttribute("id", espList[i]);
+
+                    div.appendChild(button);
+                    espcontainer.appendChild(div);
+                }
+
+        }
+    };
+    xhttp_esp.open("GET", "http://localhost:8080/setting/esp/free", true);
+    xhttp_esp.send();
 
 };
 
-// if (something) {
-//     // Manual operations (after clockpicker is initialized).
-//     $('#demo-input').clockpicker('show') // Or hide, remove ...
-//         .clockpicker('toggleView', 'minutes');
-// }
