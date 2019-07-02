c5152f4ed23e3e6104e41c2f0c2e860694721fb4
florianMarco <florian.marco@live.com>
2019-07-02 16:26:56 +0200

    - setting bar styling

--- code/frontend/resources/static/js/main.js
+++ code/frontend/resources/static/js/main.js
@@ -16,6 +16,7 @@ window.onload = function () {
         return false;
     };
 
+    showTime();
     mqttLoad();
     requestWifiList();
     requestEspFree();
@@ -35,3 +36,20 @@ window.onload = function () {
 
 
 };
+
+function showTime() {
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " ";
+    document.getElementById("clock").innerText = time;
+    document.getElementById("clock").textContent = time;
+
+    setTimeout(showTime, 1000);
+}
