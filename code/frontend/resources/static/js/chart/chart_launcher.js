49014e913fd23851eaddcd6c335aa0a1edc1ee1e
florianMarco <florian.marco@live.com>
2019-07-05 18:51:50 +0200

    - stats prese dal server

--- code/frontend/resources/static/js/chart/chart_launcher.js
+++ code/frontend/resources/static/js/chart/chart_launcher.js
@@ -1,129 +1,65 @@
-// var ctx = document.getElementById('myChart').getContext('2d');
-// var chart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//         datasets: [{
-//             label: 'Dataset 1',
-//             backgroundColor: "rgba(255,99,132,0.2)",
-//             borderColor: "rgba(255,99,132,0.2)",
-//             borderWidth: 1,
-//             data: [6, 5, 2, 8, 5, 5, 4]
-//         }, {
-//             label: 'Dataset 2',
-//             backgroundColor: "rgba(0,255,152,0.2)",
-//             borderColor: "rgba(0,255,152,0.2)",
-//             borderWidth: 1,
-//             data: [6, 5, 2, 8, 5, 5, 4]
-//         }]
-//
-//     },
-//     options: {
-//         responsive: true,
-//         maintainAspectRatio: true,
-//
-//         legend: {
-//             display: 'top',
-//         },
-//         title: {
-//             display: false,
-//         },
-//         scales: {
-//             xAxes: [{
-//                 stacked: true,
-//                 gridLines: {
-//                     display: true,
-//                     color: "rgba(255,99,132,0.2)"
-//                 }
-//             }],
-//             yAxes: [{
-//                 stacked: true,
-//                 gridLines: {
-//                     display: false
-//                 }
-//             }]
-//         }
-//     }
-// });
+function loadChart(labelArr, dataArr) {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    window.myBar = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labelArr,
+            datasets: [{
+                label: 'On time',
+                backgroundColor: "rgba(0,255,92,0.2)",
+                borderColor: "rgba(0,255,89,0.2)",
+                borderWidth: 1,
+                data: dataArr[0]
+            }, {
+                label: 'Off time',
+                backgroundColor: "rgba(255,0,20,0.2)",
+                borderColor: "rgba(255,0,21,0.2)",
+                borderWidth: 1,
+                data: dataArr[1]
+            }]
 
-// var ctx = document.getElementById('myChart').getContext('2d');
-// var chart = new Chart(ctx, {
-//     type: 'bar',
-//
-//     data: {
-//         labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
-//         datasets: [{
-//             label: "Dataset #1",
-//             backgroundColor: "rgba(255,99,132,0.2)",
-//             borderColor: "rgba(255,99,132,1)",
-//             borderWidth: 2,
-//             hoverBackgroundColor: "rgba(255,99,132,0.4)",
-//             hoverBorderColor: "rgba(255,99,132,1)",
-//             data: [6, 5, 2, 8, 5, 5, 4],
-//         },
-//         {
-//             label: "Dataset #2",
-//             backgroundColor: "rgba(0,41,255,0.2)",
-//             borderColor: "rgba(255,99,132,1)",
-//             borderWidth: 2,
-//             hoverBackgroundColor: "rgba(255,99,132,0.4)",
-//             hoverBorderColor: "rgba(255,99,132,1)",
-//             data: [6, 5, 2, 8, 5, 5, 4],
-//         }]
-//     },
-//     options: {
-//         maintainAspectRatio: false,
-//         scales: {
-//             yAxes: [{
-//                 stacked: true,
-//                 gridLines: {
-//                     display: true,
-//                     color: "rgba(255,99,132,0.2)"
-//                 }
-//             }],
-//             xAxes: [{
-//                 gridLines: {
-//                     display: false
-//                 }
-//             }]
-//         }
-//     }
-// });
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            legend: {
+                display: 'top',
+            },
+            title: {
+                display: false,
+            },
+            scales: {
+                xAxes: [{
+                    stacked: true,
+                    gridLines: {
+                        display: true,
+                        color: "rgba(255,99,132,0.2)"
+                    }
+                }],
+                yAxes: [{
+                    display: false,
+                    stacked: true,
+                    gridLines: {
+                        display: false
+                    }
+                }]
+            }
+        }
+    });
+}
 
-//
-// var data = {
-//     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
-//     datasets: [{
-//         label: "Dataset #1",
-//         backgroundColor: "rgba(255,99,132,0.2)",
-//         borderColor: "rgba(255,99,132,1)",
-//         borderWidth: 2,
-//         hoverBackgroundColor: "rgba(255,99,132,0.4)",
-//         hoverBorderColor: "rgba(255,99,132,1)",
-//         data: [65, 59, 20, 81, 56, 55, 40],
-//     }]
-// };
-//
-// var options = {
-//     maintainAspectRatio: false,
-//     scales: {
-//         yAxes: [{
-//             stacked: true,
-//             gridLines: {
-//                 display: true,
-//                 color: "rgba(255,99,132,0.2)"
-//             }
-//         }],
-//         xAxes: [{
-//             gridLines: {
-//                 display: false
-//             }
-//         }]
-//     }
-// };
-//
-// Chart.Bar('chart', {
-//     options: options,
-//     data: data
-// });
-
+function getStats() {
+    var xhttp_stats = new XMLHttpRequest();
+    xhttp_stats.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(xhttp_stats.responseText);
+            // console.log(response.dayList + "\n" + response.dataList[0] + "\n" + response.dataList[1]);
+            if (response.dayList.length !== 0)
+                loadChart(response.dayList, response.dataList);
+            else
+                console.log("No stats"); //TODO setup
+        }
+    };
+    xhttp_stats.open("GET", "http://localhost:8080/setting/stats/" + "Kitchen", true);
+    xhttp_stats.send();
+}
