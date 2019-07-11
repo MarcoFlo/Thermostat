ad5ab9e15647aaa24fb4d216a97b180f5fa94d07
florianMarco <florian.marco@live.com>
2019-07-11 21:24:44 +0200

    - script mainSensor updated

--- code/rpi_python/mainSensor.py
+++ code/rpi_python/mainSensor.py
@@ -12,7 +12,7 @@ while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None and temperature is not None:
-        client.publish("/mainSensor/sensor", temperature + "_" + humidity)
+        client.publish("/mainSensor/sensor", str(temperature) + "_" + str(humidity))
         print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
     else:
         print("Failed to retrieve data from humidity sensor")
