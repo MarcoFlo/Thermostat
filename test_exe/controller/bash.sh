af3687ceef75fbe7ad87f04a92118e56d54ff18b
florianMarco <florian.marco@live.com>
2019-07-12 15:57:39 +0200

    - rpi service

--- test_exe/controller/bash.sh
+++ test_exe/controller/bash.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+lxterminal --command java -jar controllermd-0.2.6-SNAPSHOT.jar
+lxterminal --command python3 ./mainSensor.py
+sleep 70
+lxterminal --command python3 ./mainActuator.py
 chromium-browser --incognito --noerrdialogs --disable-infobars --kiosk http://localhost:8080
-# sleep 5
-# xdotool key F11
