c6d03fcd9d69cbba7b4fd46300c39c0abc5f256b
florianMarco <florian.marco@live.com>
2019-07-12 23:19:43 +0200

    chrome

--- test_exe/controller/chromium.sh
+++ test_exe/controller/chromium.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sleep 10
-lxterminal --command echo ciao
-lxterminal --command chromium-browser --incognito --noerrdialogs --disable-infobars --kiosk http://localhost:8080
+sleep 30
+chromium-browser --incognito --noerrdialogs --disable-infobars --kiosk http://localhost:8080
