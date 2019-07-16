d70bb9ab2be491d530dd8fc54c8e475caa713785
florianMarco <florian.marco@live.com>
2019-07-16 16:19:07 +0200

    - wf

--- code/backend/controllermd/src/main/java/it/polito/thermostat/controllermd/resources/MQTTaws/PingAWS.java
+++ code/backend/controllermd/src/main/java/it/polito/thermostat/controllermd/resources/MQTTaws/PingAWS.java
@@ -23,7 +23,7 @@ public class PingAWS {
 
     public PingAWS(int sequence) {
         this.event_id = 1;
-        this.timestamp = LocalDateTime.now().withNano(1000).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+        this.timestamp = LocalDateTime.now().withNano(1000).toString();
         this.device_mac = HostAddressGetter.getMAC();
         event = new Event("Ping response", sequence + 1);
     }
